c0dfd01cba39ba927c997c8226f43c3793b69387
kirill kurenev <cirill@mail.ru>
2022-08-20 13:06:59 +0300

    fourth task add

--- Program.cs
+++ Program.cs
@@ -66,3 +66,12 @@ else
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+Console.WriteLine("Задача 4");
+Console.Write("Введите введите конечное число ряда: ");
+int nNumb = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= nNumb)
+{
+    if (count%2==0) Console.Write(count+" ");
+    count++;
+}
